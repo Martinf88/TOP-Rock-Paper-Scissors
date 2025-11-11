cc06142494de9bc4b0d8a34cb75db793a246347c
Martinf88 <143597591+Martinf88@users.noreply.github.com>
2025-11-11 04:19:44 +0100

    removes psudocode

--- rps.js
+++ rps.js
@@ -1,14 +1,3 @@
-// Create function getComputerChoice
-// Create variable named computorChoice to store randomized value in
-// Create function getRandomNumber that generates and returns a random number
-// between 1-3
-// IF number is 1 return "Rock"
-// ELSE IF number is 2 return "Paper"
-// ELSE number is 3 return "Scissors"
-// Return randomized value
-// Call function and console.log return value to check if it works as expected
-
-// Generates random number between 0-2
 const getRandomNumber = () => {
   return Math.floor(Math.random() * 3);
 };
@@ -31,12 +20,6 @@ const getComputerChoice = () => {
   return computorChoice;
 };
 
-// Create named getHumanChoice that prompts users for a number between 1-3
-// IF null return no choice made
-// IF other that 1-3 prompt again asking for valig input
-// IF 1 return Rock, if 2 return Paper and if 3 return Scissors
-// Create a variable named humanChoice
-
 const getHumanChoice = () => {
   let humanChoice =
     Number(prompt("Enter 1 for Rock, 2 for Paper or 3 for Scissors")) || null;
@@ -55,24 +38,6 @@ const getHumanChoice = () => {
 
   return humanChoice;
 };
-
-// Create function playRound that takes two arguments, humanChoice and computerChoice
-// Define variables humanSelection and computorSelection to equal getHumanChoice and getComputerChoice
-// IF statement for gamelogic, Rock beats Scissors, Scissors beats Paper, Paper beats Rock
-// Log Result of who wone
-// Update humanScore or computerScore
-
-/*Create a new function named playGame.
-Move your playRound function and score variables so that they’re declared inside of the new playGame function
-Play 5 rounds by calling playRound 5 times.
-Hint: When you assign a function call to a variable, the return value of that function is assigned to the variable. Accessing the variable afterward will only provide the assigned value; it doesn’t recall the function. You need to recall the choice functions to get new choices for each round.
-Re-work your previous functions or create more helper functions if necessary. Specifically, you may want to change the return values to something more useful.
-If you already know about loops, you can use them. If not, don’t worry! Loops will be covered in the next lesson.*/
-
-// Create function named playGame.
-// Move playRound function and score variables inside playGame function.
-// Create a loop and call playRound 5 times and keep track of score
-// Log out the winner based on the highest score
 
 const playGame = () => {
   let computerScore = 0;
