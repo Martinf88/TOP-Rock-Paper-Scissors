91e5b125635cdd0b4731e82bd91a4ac722a90608
Martinf88 <143597591+Martinf88@users.noreply.github.com>
2025-11-17 05:27:36 +0100

    Adds button functionality to the ui eventlistener to the buttons and changes getHumanChoice to use a swith statement

--- rps.js
+++ rps.js
@@ -1,3 +1,5 @@
+const choiceBtns = document.querySelectorAll("button");
+
 const getRandomNumber = () => {
   return Math.floor(Math.random() * 3);
 };
@@ -20,20 +22,23 @@ const getComputerChoice = () => {
   return computorChoice;
 };
 
-const getHumanChoice = () => {
-  let humanChoice =
-    Number(prompt("Enter 1 for Rock, 2 for Paper or 3 for Scissors")) || null;
+const getHumanChoice = (id) => {
+  console.log(id);
 
-  if (humanChoice === null) {
-    return "No choice made.";
-  } else if (![1, 2, 3].includes(humanChoice)) {
-    return prompt("wrong entry, try again.");
-  } else if (humanChoice === 1) {
-    humanChoice = "Rock";
-  } else if (humanChoice === 2) {
-    humanChoice = "Paper";
-  } else if (humanChoice === 3) {
-    humanChoice = "Scissors";
+  let humanChoice = "";
+
+  switch (id) {
+    case "rock":
+      humanChoice = "Rock";
+      break;
+    case "paper":
+      humanChoice = "Paper";
+      break;
+    case "scissors":
+      humanChoice = "Scissors";
+      break;
+    default:
+      break;
   }
 
   return humanChoice;
@@ -45,38 +50,69 @@ const playGame = () => {
 
   const playRound = (humanChoice, computerChoice) => {
     const scoreMessage = `You: ${humanScore}. Computer: ${computerScore}`;
+
+    let winMessage = "";
     const humanWinMessage = `You Won! ${humanChoice} beats ${computerChoice} Score: ${scoreMessage}`;
     const computerWinMessage = `You lost! ${computerChoice} beats ${humanChoice} Score: ${scoreMessage}`;
 
-    if (humanChoice === "Rock" && computerChoice === "Scissors") {
-      humanScore++;
+    switch (humanChoice) {
+      case "Rock":
+        if (computerChoice === "Paper") {
+          computerScore++;
+          winMessage = computerWinMessage;
+        } else if (computerChoice === "Scissors") {
+          humanScore++;
+          winMessage = humanWinMessage;
+        } else {
+          winMessage = `Draw! ${computerChoice} and ${humanChoice}`;
+        }
+        break;
+      case "Paper":
+        if (computerChoice === "Scissors") {
+          computerScore++;
+          winMessage = computerWinMessage;
+        } else if (computerChoice === "Rock") {
+          humanScore++;
+          winMessage = humanWinMessage;
+        } else {
+          winMessage = `Draw! ${computerChoice} and ${humanChoice}`;
+        }
 
-      return humanWinMessage;
-    } else if (humanChoice === "Paper" && computerChoice === "Rock") {
-      humanScore++;
-      return humanWinMessage;
-    } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
-      humanScore++;
-      return humanWinMessage;
-    } else if (computerChoice === "Rock" && humanChoice === "Scissors") {
-      computerScore++;
-      return computerWinMessage;
-    } else if (computerChoice === "Paper" && humanChoice === "Rock") {
-      computerScore++;
-      return computerWinMessage;
-    } else if (computerChoice === "Paper" && humanChoice === "Rock") {
-      computerScore++;
-      return computerWinMessage;
-    } else if (computerChoice === humanChoice) {
-      return `Draw! ${computerChoice} and ${humanChoice}`;
+        break;
+      case "Scissors":
+        if (computerChoice === "Rock") {
+          computerScore++;
+          winMessage = computerWinMessage;
+        } else if (computerChoice === "Paper") {
+          humanScore++;
+          winMessage = humanWinMessage;
+        } else {
+          winMessage = `Draw! ${computerChoice} and ${humanChoice}`;
+        }
+        break;
+
+      default:
+        break;
     }
+
+    return winMessage;
   };
 
-  for (i = 0; i < 5; i++) {
-    const humanSelection = getHumanChoice();
-    const computorSelection = getComputerChoice();
-    console.log(playRound(humanSelection, computorSelection));
-  }
+  // Add an eventlistener to all choiceBtns
+  // Create const humanSelection,
+  // it should call getHumanChoice and send event.target
+  // Create const computorSelection = getComputorChoice
+  // log playRound(humanSelection, computorSelection)
+
+  choiceBtns.forEach((btn) =>
+    btn.addEventListener("click", function (e) {
+      const humanSelection = getHumanChoice(e.target.id);
+
+      const computorSelection = getComputerChoice();
+      console.log(humanSelection, computorSelection);
+      console.log(playRound(humanSelection, computorSelection));
+    })
+  );
 
   const winnerIsComp = `Game Over. Computer won ${computerScore} to ${humanScore}`;
   const winnerIsHuman = `Game Over. You won ${humanScore} to ${computerScore}`;
